d490c9d4b5c9b796138c181e088b1c2e7e7ff9e9
Showmick Das <showmickdas75@gmail.com>
2024-12-20 19:25:46 -0500

    processing the data

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,5 +1,33 @@
 import pandas as pd
-data = pd.read_csv("data/emissions_data.csv", index_col=0)
-print(data.head())
-print(data.isnull().sum())
-filtered_data = data.loc[:,["Manufacturer,"]]
+emissions_data = pd.read_csv("data\emissions_data.csv")
+
+# As many manufacturers in the data set don't have valid entries before 2008, to maintain consistency, we will only work with the data from 2008 onwards
+emissions_data['Model Year'] = pd.to_numeric(emissions_data['Model Year'], errors='coerce')
+data_2008 = emissions_data[emissions_data['Model Year'] >= 2008]
+
+# Separating the data for all the manufacturers combined, and the separate unique manufacturers
+all_manufacturers_data = data_2008[data_2008['Manufacturer'] == 'All']
+specific_manufacturers_data = data_2008[data_2008['Manufacturer'] != 'All']
+
+# Filtering the relevant columns for our analysis
+relevant_columns = [
+    'Manufacturer', 'Model Year', 'Vehicle Type', 'Production (000)', 
+    'Real-World MPG', 'Real-World CO2 (g/mi)', 'Ton-MPG (Real-World)', 'Weight (lbs)',
+    'Powertrain - Diesel', 
+    'Powertrain - Battery Electric Vehicle (BEV)',
+    'Powertrain - Plug-in Hybrid Electric Vehicle (PHEV)',
+    'Powertrain - Fuel Cell Electric Vehicle (FCEV)',
+    'Powertrain - Other (incl. CNG)',
+    'Powertrain - Gasoline Mild Hybrid/MHEV',
+    'Powertrain - Gasoline Strong Hybrid/HEV',
+    'Powertrain - Gasoline with Start/Stop',
+    'Powertrain - Gasoline without Start/Stop'
+]
+
+# Filter the dataset for specific manufacturers (excluding "All") and relevant columns
+specific_manufacturers_filtered = specific_manufacturers_data[relevant_columns].copy()
+
+print(specific_manufacturers_filtered.head())
+
+if specific_manufacturers_filtered.empty:
+    print("No data available for plotting.")
